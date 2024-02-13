b2f527779645595a7ea165bc1cd365ff32eae949
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-02-14 00:04:02 +0530

    req/response

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('suryansh');
+    const url=req.url
+    if(url==='/home'){
+        res.setHeader('Content-type','text/html');
+        res.write('<html>')
+        res.write('<head>')
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('</head>')
+        res.write('</html>')
+    }
+    if(url==='/about'){
+        res.setHeader('Content-type','text/html');
+        res.write('<html>')
+        res.write('<head>')
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</head>')
+        res.write('</html>')
+    }
+    if(url==='/node'){
+        res.setHeader('Content-type','text/html');
+        res.write('<html>')
+        res.write('<head>')
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+        res.write('</head>')
+        res.write('</html>')
+    }
 });
 
 server.listen(4000);
