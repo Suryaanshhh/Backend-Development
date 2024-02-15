402c17b8497f58b6ced5f5746493e2bd30d595c3
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-02-15 15:24:36 +0530

    body parser

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 
 const express=require('express')
 const app=express();
-const route=require('./route')
+const parsedBody=require('body-parser')
+app.use(parsedBody.urlencoded({extended:false}))
 
-app.use((req,res,next)=>{
-    console.log('In the middlewares')
-    next()
+app.use('/add-product',(req,res,next)=>{
+   res.send('<form action="/product" method="POST"><input type="text" name="Product"><input type="number" name="Quantity"><button type="submit">ADD</button>')
 })
-app.use((req,res,next)=>{
-    res.send( { key1: "hello" })
-    console.log('In the new middlewares')
+
+app.use('/product',(req,res,next)=>{
+   console.log(req.body)
+   res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>PRODUCT ADDED</h1>')
 })
 app.listen(4000)
